12be76ecddcb717be4e434f611d43eb7a097b22c
Renata Gjoreska <renata.gjoreska@gmail.com>
2016-02-23 22:09:07 +0100

    Example code

--- Planner/Planner.cpp
+++ Planner/Planner.cpp
@@ -3,15 +3,50 @@
 #include "headers.h"
 #include <emscripten.h>
 
+#include <emscripten/bind.h>
+using namespace emscripten;
 
-// extern "C" int main(int argc)
-// {
-// 	std::printf("Hello world");
-// 	return 0;
-// }
+class Example
+{
+public:
+	Example(int number, std::string text)
+		: m_number(number)
+		, m_text(text)
+	{};
 
-// extern "C" int sum(int x, int y)
-// {
-// 	printf("Sum invoked\n");
-// 	return x + y;
-// }
+	int getNumber() const { return m_number; }
+	void setNumber(int val) { m_number = val; }
+
+	std::string getText() const { return m_text; }
+	void setText(std::string val) { m_text = val; }
+
+	int squareNumber() { return m_number * m_number; }
+
+	static std::string greeting() { return "Hello all from static method"; }
+
+	static void justCallJavaScript()
+	{
+		int x = EM_ASM_INT({
+			Module.print('I received: ' + $0);
+			return $0 + 5;
+		}, 100);
+
+		printf("JavaScript called, now result is %d \n", x);
+	}
+
+private:
+	int m_number;
+	std::string m_text;
+};
+
+// Binding code
+EMSCRIPTEN_BINDINGS(planner) {
+	class_<Example>("Example")
+		.constructor<int, std::string>()
+		.function("squareNumber", &Example::squareNumber)
+		.class_function("greeting", &Example::greeting)
+		.class_function("justCallJavaScript", &Example::justCallJavaScript)
+		.property("m_number", &Example::getNumber, &Example::setNumber)
+		.property("m_text", &Example::getText, &Example::setText)
+		;
+}
